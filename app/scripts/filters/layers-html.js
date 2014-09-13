f589593926ab3cdba7453d8c29e7676677853632
Federico Brigante <bfred-it@users.noreply.github.com>
2014-09-13 15:14:15 +0200

    Made template logic-less

--- app/scripts/filters/layers-html.js
+++ app/scripts/filters/layers-html.js
@@ -4,7 +4,45 @@ var _ = require('lodash');
 var template = require('./layers-html.html');
 var formatHTML = _.template(template);
 module.exports = function() {
-  return function(input) {
-  	return formatHTML(input);
-  };
+	return function(ls) {
+		ls.__ = {};
+
+		if (ls.opts.output.useTrimmedCanvas) {
+			ls.__.width = ls.trimmed.width;
+			ls.__.height = ls.trimmed.height;
+		} else {
+			ls.__.width = ls.width;
+			ls.__.height = ls.height;
+		}
+		ls.__.style = [
+			'position: relative;',
+			'width: ' + ls.__.width + 'px;',
+			'height: ' + ls.__.height + 'px;'
+		];
+
+		if (ls.opts.output.attribute && ls.opts.output.namespace) {
+			ls.__.idAttr = ls.opts.output.attribute + '="' + ls.opts.output.namespace.replace(/[-_]+$/,'') + '" ';
+		}
+
+		_.forEach(ls.list, function (layer) {
+			layer._ = {};
+
+			layer._.style = [
+				'position: absolute;',
+				'top: ' + layer.coordinates.get('y') + 'px;',
+				'left: ' + layer.coordinates.get('x') + 'px;'
+			];
+
+			if (ls.opts.output.attribute) {
+				layer._.idAttr = ls.opts.output.attribute + '="' + (ls.opts.output.namespace?ls.opts.output.namespace:'')+layer.name + '" ';
+			}
+
+			if (ls.opts.output.namespace) {
+				layer._.filename = ls.opts.output.namespace.replace(/[-_]+$/,'').replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()+'/' + layer.file.name;
+			} else {
+				layer._.filename = layer.file.name;
+			}
+		});
+		return formatHTML(ls);
+	};
 };
