f87ab9563fea9d89e760ab15a4cfd493bd38226b
Federico Brigante <bfred-it@users.noreply.github.com>
2014-09-13 04:57:29 +0200

    Notes, test, removed unnecessary file

--- app/scripts/bfred.js
+++ app/scripts/bfred.js
@@ -3,6 +3,6 @@ require('angular');
 
 var slayer = angular.module('slayer', ['angularFileUpload','ui.tree', 'ngStorage']);
 slayer.controller('SlayerController', require('./controllers/slayer.js'));
-slayer.filter('layersHTML', require('./filters/layers-html.js'));
+slayer.filter('layersHTML', require('./filters/layers-html.js'));//http://currentlyunderdevelopment.blogspot.it/2013/03/recursive-handlebars-templates-made-easy.html
 slayer.directive('vbox', require('./directives/svg-viewbox.js'));
 // slayer.directive('ToggleGroupDirective', require('./directives/toggle-group.js'));
